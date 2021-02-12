f499de621fc56b04dde1dc35da077d4deb03dbff
Bruno <brmcerqueira@gmail.com>
2021-02-12 15:34:22 -0300

    Adjust

--- tests/isDate_test.ts
+++ tests/isDate_test.ts
@@ -1,5 +1,5 @@
 import { assert } from "https://deno.land/std/testing/asserts.ts";
-import { required, validate, isDate } from "../mod.ts";
+import { required, validate, isDate, dateAfterOrEqual, dateBeforeOrEqual } from "../mod.ts";
 
 Deno.test("isDate", async () => {
     let data = {
@@ -9,7 +9,7 @@ Deno.test("isDate", async () => {
 
     let result = await validate(data, {
         dateOne: [required, isDate],
-        dateTwo: [required, isDate]
+        dateTwo: [required, isDate, dateAfterOrEqual(new Date(2000, 0, 1)), dateBeforeOrEqual(new Date(2000, 0, 2))]
     })
 
     assert(result.valid);
@@ -22,6 +22,30 @@ Deno.test("isDate - fail", async () => {
 
     let result = await validate(data, {
         dateOne: [required, isDate]
+    })
+
+    assert(!result.valid);
+});
+
+Deno.test("isDate - dateAfterOrEqual - fail", async () => {
+    let data = {
+        dateOne: "1999-01-01T12:00:00.000Z"
+    }
+
+    let result = await validate(data, {
+        dateOne: [required, isDate, dateAfterOrEqual(new Date(2000, 0, 1))]
+    })
+
+    assert(!result.valid);
+});
+
+Deno.test("isDate - dateBeforeOrEqual - fail", async () => {
+    let data = {
+        dateOne: "2000-01-02T12:00:00.000Z"
+    }
+
+    let result = await validate(data, {
+        dateOne: [required, isDate, dateBeforeOrEqual(new Date(2000, 0, 1))]
     })
 
     assert(!result.valid);
